5420c0e31c34b6cf4ca26f9a291a0002d995528b
Carl Philipp Klemm <carl@uvos.xyz>
2018-01-08 22:51:02 +0100

    Ringbuffer Changes requierd by serial

--- src/ringbuffer.h
+++ src/ringbuffer.h
@@ -15,19 +15,22 @@ private:
 public:
     
     RingBuffer(){}
+    
     bool isEmpty()
     {
         return _tailIndex >= _headIndex;
     }
+    
     uint8_t read()
     {
-    if(!isEmpty())
-    {
-        _tailIndex++;
-        return _buffer[(_tailIndex - 1) % BUFFER_SIZE];
+        if(!isEmpty())
+        {
+            _tailIndex++;
+            return _buffer[(_tailIndex - 1) % BUFFER_SIZE];
+        }
+        else return '\0';
     }
-    else return '\0';
-    }
+    
     void read( uint8_t* buffer, unsigned int length )
     {
         for(uint8_t i = 0; i < length; i++)
@@ -35,6 +38,7 @@ public:
             buffer[i] = read();
         }
     }
+    
     void write( uint8_t in )
     {
         if (_headIndex - BUFFER_SIZE > 0 && _tailIndex - BUFFER_SIZE > 0)
@@ -45,8 +49,42 @@ public:
         _buffer[_tailIndex] = in;
         _tailIndex++;
     }
+    
     void write( uint8_t* buffer, unsigned int length )
     {
         for(uint8_t i = 0; i < length; i++) write(buffer[i]);
+    }
+    
+    void flush()
+    {
+        _headIndex = 0;
+        _tailIndex = 0;
+        for(int i = 0; i < BUFFER_SIZE; i++) _buffer[i] = ' ';
+    }
+    
+    unsigned int getString(uint8_t terminator, char* buffer, const int bufferLength)
+    {
+        int i = 0;
+        for(; i <= (_headIndex-_tailIndex) && i <= BUFFER_SIZE && _buffer[(_tailIndex+i) % BUFFER_SIZE] != terminator; i++);
+        
+        if( i < (_headIndex-_tailIndex) && i > 0)
+        {
+            int j = 0;
+            for(; j < i && j < bufferLength-1 ; j++)
+            {
+                buffer[j] = read();
+            }
+            buffer[j+1]='\0';
+            _tailIndex++;
+        }
+        else
+        {
+            i = 0;
+            if( _tailIndex >= (32768) - 2*BUFFER_SIZE ) flush();
+        }
+
+        if (_buffer[(_tailIndex+i) % BUFFER_SIZE] == terminator) _tailIndex++;
+        
+        return i;
     }
 };
