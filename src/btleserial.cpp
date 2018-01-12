aed51e8a2265c21fcdc8e5dff2e936408fc85d92
Carl Philipp Klemm <carl@uvos.xyz>
2018-01-12 11:28:27 +0100

    Fix Btle advertising and fix connected count functionality

--- src/btleserial.cpp
+++ src/btleserial.cpp
@@ -19,6 +19,8 @@
 
 #include "ringbuffer.h"
 
+extern uint8_t __data_start__;    
+
 //sdk includes
 extern "C"
 {
@@ -45,36 +47,36 @@ extern "C"
                                  .rc_temp_ctiv  = 0,                                \
                                  .xtal_accuracy = NRF_CLOCK_LF_XTAL_ACCURACY_20_PPM}
 
-
 //these defines where lifted from the NORDIC SDK NRF UART BTLE example.
-#define NUS_SERVICE_UUID_TYPE           BLE_UUID_TYPE_VENDOR_BEGIN 
-#define DEVICE_NAME                     "beaconName"                         /**< Name of device. Will be included in the advertising data. */
 
-#define APP_BLE_CONN_CFG_TAG            1                                       /**< A tag identifying the SoftDevice BLE configuration. */
-#define LINK_TOTAL                      NRF_SDH_BLE_PERIPHERAL_LINK_COUNT + NRF_SDH_BLE_CENTRAL_LINK_COUNT
+#define APP_TIMER_PRESCALER             0 
 
-#define APP_ADV_INTERVAL                MSEC_TO_UNITS(100, UNIT_0_625_MS)        /**< The advertising interval (in units of 0.625 ms; this value corresponds to 40 ms). */
-#define APP_ADV_TIMEOUT_IN_SECONDS      BLE_GAP_ADV_TIMEOUT_GENERAL_UNLIMITED   /**< The advertising time-out (in units of seconds). When set to 0, we will never time out. */
+#define IS_SRVC_CHANGED_CHARACT_PRESENT 0                                           /**< Include the service_changed characteristic. If not enabled, the server's database cannot be changed for the lifetime of the device. */
 
-#define APP_TIMER_PRESCALER             0                                           /**< Value of the RTC1 PRESCALER register. */
-#define APP_TIMER_OP_QUEUE_SIZE         4                                           /**< Size of timer operation queues. */
+#if (NRF_SD_BLE_API_VERSION == 3)
+#define NRF_BLE_MAX_MTU_SIZE            GATT_MTU_SIZE_DEFAULT                       /**< MTU size used in the softdevice enabling and to reply to a BLE_GATTS_EVT_EXCHANGE_MTU_REQUEST event. */
+#endif
 
-#define MIN_CONN_INTERVAL               MSEC_TO_UNITS(100, UNIT_1_25_MS)        /**< Minimum acceptable connection interval (0.5 seconds). */
-#define MAX_CONN_INTERVAL               MSEC_TO_UNITS(200, UNIT_1_25_MS)        /**< Maximum acceptable connection interval (1 second). */
-#define SLAVE_LATENCY                   0                                       /**< Slave latency. */
-#define CONN_SUP_TIMEOUT                MSEC_TO_UNITS(4000, UNIT_10_MS)         /**< Connection supervisory time-out (4 seconds). */
-
-#define FIRST_CONN_PARAMS_UPDATE_DELAY APP_TIMER_TICKS(15000, APP_TIMER_PRESCALER)                  /**< Time from initiating event (connect or start of notification) to first time sd_ble_gap_conn_param_update is called (15 seconds). */
-#define NEXT_CONN_PARAMS_UPDATE_DELAY    APP_TIMER_TICKS(30000, APP_TIMER_PRESCALER)                  /**< Time between each call to sd_ble_gap_conn_param_update after the first call (5 seconds). */
-#define MAX_CONN_PARAMS_UPDATE_COUNT    3                                       /**< Number of attempts before giving up the connection parameter negotiation. */
-
-#define APP_FEATURE_NOT_SUPPORTED       BLE_GATT_STATUS_ATTERR_APP_BEGIN + 2    /**< Reply when unsupported features are requested. */
-
+#define APP_FEATURE_NOT_SUPPORTED       BLE_GATT_STATUS_ATTERR_APP_BEGIN + 2        /**< Reply when unsupported features are requested. */
 
 #define CENTRAL_LINK_COUNT              0                                           /**< Number of central links used by the application. When changing this number remember to adjust the RAM settings*/
-#define PERIPHERAL_LINK_COUNT           1
+#define PERIPHERAL_LINK_COUNT           1                                           /**< Number of peripheral links used by the application. When changing this number remember to adjust the RAM settings*/
 
-#define NRF_BLE_MAX_MTU_SIZE            GATT_MTU_SIZE_DEFAULT 
+#define NUS_SERVICE_UUID_TYPE           BLE_UUID_TYPE_VENDOR_BEGIN                  /**< UUID type for the Nordic UART Service (vendor specific). */
+
+#define APP_ADV_INTERVAL                64                                          /**< The advertising interval (in units of 0.625 ms. This value corresponds to 40 ms). */
+#define APP_ADV_TIMEOUT_IN_SECONDS      180                                         /**< The advertising timeout (in units of seconds). */
+
+
+
+#define MIN_CONN_INTERVAL               MSEC_TO_UNITS(20, UNIT_1_25_MS)             /**< Minimum acceptable connection interval (20 ms), Connection interval uses 1.25 ms units. */
+#define MAX_CONN_INTERVAL               MSEC_TO_UNITS(75, UNIT_1_25_MS)             /**< Maximum acceptable connection interval (75 ms), Connection interval uses 1.25 ms units. */
+#define SLAVE_LATENCY                   0                                           /**< Slave latency. */
+#define CONN_SUP_TIMEOUT                MSEC_TO_UNITS(4000, UNIT_10_MS)             /**< Connection supervisory timeout (4 seconds), Supervision Timeout uses 10 ms units. */
+#define FIRST_CONN_PARAMS_UPDATE_DELAY  APP_TIMER_TICKS(5000, APP_TIMER_PRESCALER)  /**< Time from initiating event (connect or start of notification) to first time sd_ble_gap_conn_param_update is called (5 seconds). */
+#define NEXT_CONN_PARAMS_UPDATE_DELAY   APP_TIMER_TICKS(30000, APP_TIMER_PRESCALER) /**< Time between each call to sd_ble_gap_conn_param_update after the first call (30 seconds). */
+#define MAX_CONN_PARAMS_UPDATE_COUNT    3                                           /**< Number of attempts before giving up the connection parameter negotiation. */
+
 
 /**@brief   Priority of the application BLE event handler.
  * @note    You shouldn't need to modify this value.
@@ -92,20 +94,25 @@ static ble_uuid_t                       m_adv_uuids[] = {{BLE_UUID_NUS_SERVICE, 
 
 RingBuffer<128> ringBuffer; //Bluetooth ring buffer.
 
-BtleUart::BtleUart()
+BtleSerial::BtleSerial(Serial* serial): _serial(serial)
 {
-    APP_TIMER_INIT(APP_TIMER_PRESCALER, APP_TIMER_OP_QUEUE_SIZE, false);
+    serial->write(" app timer init\n");
+    
+    serial->write(" stack init\n");
     _bleStackInit();
-    ble_conn_state_init();
+    serial->write(" gap init\n");
     _gapParamsInit();
     //nrf_ble_gatt_init(&m_gatt, NULL);
+    serial->write(" services init\n");
     _servicesInit();
+    serial->write(" avertising init\n");
     _advertisingInit();
+    serial->write(" connection paramaters init\n");
     _connectionParamatersInit();
 }
 
 //Enable Advertising
-void BtleUart::start()
+void BtleSerial::start()
 {
     ble_gap_adv_params_t adv_params;
     
@@ -120,61 +127,75 @@ void BtleUart::start()
     sd_ble_gap_adv_start(&adv_params);
 }
 
-void BtleUart::_bleStackInit()
+void BtleSerial::_bleStackInit()
 {
      uint32_t err_code;
 
     nrf_clock_lf_cfg_t clock_lf_cfg = NRF_CLOCK_LFCLKSRC;
 
-    // Initialize SoftDevice.
-    SOFTDEVICE_HANDLER_INIT(&clock_lf_cfg, NULL);
+    _serial->write("  softdevice_handler_init");
+    
+    static uint32_t ble_evt_buffer[CEIL_DIV(BLE_STACK_EVT_MSG_BUF_SIZE, sizeof(uint32_t))];                                                                         
+    err_code = softdevice_handler_init(&clock_lf_cfg,ble_evt_buffer, sizeof(ble_evt_buffer), NULL); 
+    
+    _serial->write(" error code: ");
+    _serial->write(err_code);
+    _serial->putChar('\n');
+    APP_ERROR_CHECK(err_code);       
 
     ble_enable_params_t ble_enable_params;
+    _serial->write("  softdevice_enable_get_default_config\n");
     err_code = softdevice_enable_get_default_config(CENTRAL_LINK_COUNT,
                                                     PERIPHERAL_LINK_COUNT,
                                                     &ble_enable_params);
     APP_ERROR_CHECK(err_code);
 
     //Check the ram settings against the used number of links
+    _serial->write("  Ram addr\n");
     CHECK_RAM_START_ADDR(CENTRAL_LINK_COUNT,PERIPHERAL_LINK_COUNT);
 
+    /*uint32_t app_ram_base = (uint32_t) &__data_start__;
+    err_code = sd_ble_enable(&ble_enable_params, &app_ram_base);
+
+    uint32_t correct_ram = app_ram_base;
+    uint32_t current_ram = (uint32_t) &__data_start__;
+    _serial->write("   ram schould be ");
+    _serial->write(correct_ram);
+    _serial->write(" is ");
+    _serial->write(current_ram);
+     _serial->putChar('\n');*/
+
     // Enable BLE stack.
-#if (NRF_SD_BLE_API_VERSION == 3)
     ble_enable_params.gatt_enable_params.att_mtu = NRF_BLE_MAX_MTU_SIZE;
-#endif
+    _serial->write("  softdevice_enable");
     err_code = softdevice_enable(&ble_enable_params);
+    _serial->write(" error code: ");
+    _serial->write(err_code);
+    _serial->putChar('\n');
     APP_ERROR_CHECK(err_code);
 
     // Subscribe for BLE events.
+    _serial->write("  softdevice_ble_evt_handler_set");
     err_code = softdevice_ble_evt_handler_set(_btleEventHandler);
+    _serial->write(" error code: ");
+    _serial->write(err_code);
+    _serial->putChar('\n');
     APP_ERROR_CHECK(err_code);
-    /*
-  nrf_sdh_enable_request();
-
-  // Configure the BLE stack using the default settings.
-  // Fetch the start address of the application RAM.
-  uint32_t ram_start = 0;
-  nrf_sdh_ble_default_cfg_set(APP_BLE_CONN_CFG_TAG, &ram_start);
-
-  // Enable BLE stack.
-  nrf_sdh_ble_enable(&ram_start);
-
-  // Register a handler for BLE events.
-  NRF_SDH_BLE_OBSERVER(m_ble_observer, APP_BLE_OBSERVER_PRIO, _btleEventHandler, NULL);
-  */
 }
 
 
 //Set Generic Access Profile info
-void BtleUart::_gapParamsInit()
+void BtleSerial::_gapParamsInit()
 {
+  uint32_t                err_code;
   ble_gap_conn_params_t   gap_conn_params;
   ble_gap_conn_sec_mode_t sec_mode;
 
   BLE_GAP_CONN_SEC_MODE_SET_OPEN(&sec_mode);
 
-  sd_ble_gap_device_name_set(&sec_mode, (const uint8_t *)DEVICE_NAME, strlen(DEVICE_NAME));
-
+  err_code =  sd_ble_gap_device_name_set(&sec_mode, (const uint8_t *)"UVOS Telemetry System", strlen( "UVOS Telemetry System"));
+    APP_ERROR_CHECK(err_code);
+  
   memset(&gap_conn_params, 0, sizeof(gap_conn_params));
 
   gap_conn_params.min_conn_interval = MIN_CONN_INTERVAL;
@@ -182,22 +203,26 @@ void BtleUart::_gapParamsInit()
   gap_conn_params.slave_latency     = SLAVE_LATENCY;
   gap_conn_params.conn_sup_timeout  = CONN_SUP_TIMEOUT;
 
-  sd_ble_gap_ppcp_set(&gap_conn_params);
+  
+  err_code = sd_ble_gap_ppcp_set(&gap_conn_params);
+  APP_ERROR_CHECK(err_code);
 }
 
-void BtleUart::_servicesInit()
+void BtleSerial::_servicesInit()
 {
+    uint32_t       err_code;
     ble_nus_init_t nus_init;
 
     memset(&nus_init, 0, sizeof(nus_init));
 
     nus_init.data_handler = _btleDataHandler;
 
-    ble_nus_init(&m_nus, &nus_init);
+    err_code = ble_nus_init(&m_nus, &nus_init);
+    APP_ERROR_CHECK(err_code);
 }
 
 //initalize btle advertising
-void BtleUart::_advertisingInit()
+void BtleSerial::_advertisingInit()
 {
     ble_advdata_t          advdata;
     ble_advdata_t          scanrsp;
@@ -218,17 +243,19 @@ void BtleUart::_advertisingInit()
     options.ble_adv_fast_interval = APP_ADV_INTERVAL;
     options.ble_adv_fast_timeout  = APP_ADV_TIMEOUT_IN_SECONDS;
 
-    ble_advertising_init(&advdata, &scanrsp, &options, NULL, NULL);
+    uint32_t err_code = ble_advertising_init(&advdata, &scanrsp, &options, NULL, NULL);
+    APP_ERROR_CHECK(err_code);
 }
 
-void BtleUart::_connParamsEvtHandler(ble_conn_params_evt_t * event)
+void BtleSerial::_connParamsEvtHandler(ble_conn_params_evt_t * event)
 {
     if (event->evt_type == BLE_CONN_PARAMS_EVT_FAILED) sd_ble_gap_disconnect(m_conn_handle, BLE_HCI_CONN_INTERVAL_UNACCEPTABLE);
 }
 
 //initalize CP module;
-void BtleUart::_connectionParamatersInit()
+void BtleSerial::_connectionParamatersInit()
 {
+    
     ble_conn_params_init_t cp_init;
 
     memset(&cp_init, 0, sizeof(cp_init));
@@ -242,10 +269,11 @@ void BtleUart::_connectionParamatersInit()
     cp_init.evt_handler                    = _connParamsEvtHandler;
     cp_init.error_handler                  = _connParamsErrorHandler;
 
-    ble_conn_params_init(&cp_init);
+    uint32_t err_code  = ble_conn_params_init(&cp_init);
+    APP_ERROR_CHECK(err_code);
 }
 
-void BtleUart::_connected(const ble_gap_evt_t * const p_gap_evt)
+void BtleSerial::_connected(const ble_gap_evt_t * const p_gap_evt)
 {
   uint32_t    periph_link_cnt = ble_conn_state_n_peripherals(); // Number of peripheral links.
 
@@ -255,21 +283,21 @@ void BtleUart::_connected(const ble_gap_evt_t * const p_gap_evt)
   else
   {
     // Continue advertising. More connections can be established because the maximum link count has not been reached.
-    BtleUart::start();
+    BtleSerial::start();
   }
 }
 
-void BtleUart::_disconnected(ble_gap_evt_t const * const p_gap_evt)
+void BtleSerial::_disconnected(ble_gap_evt_t const * const p_gap_evt)
 {
   uint32_t    periph_link_cnt = ble_conn_state_n_peripherals(); // Number of peripheral links.
   if (periph_link_cnt == (PERIPHERAL_LINK_COUNT - 1))
   {
     // Advertising is not running when all connections are taken, and must therefore be started.
-    BtleUart::start();
+    BtleSerial::start();
   }
 }
 
-void BtleUart::_onAuthorizeRequest(ble_evt_t const * p_ble_evt)
+void BtleSerial::_onAuthorizeRequest(ble_evt_t const * p_ble_evt)
 {
   ret_code_t                            err_code;
   ble_gatts_evt_rw_authorize_request_t  req;
@@ -299,18 +327,25 @@ void BtleUart::_onAuthorizeRequest(ble_evt_t const * p_ble_evt)
 }
 
 
+void BtleSerial::_btleDataHandler(ble_nus_t* p_nus, uint8_t* data, uint16_t length)
+{
+    ringBuffer.write(data, length);
+}
+
+
 /**@brief Function for handling BLE events.
  *
  * @param[in]   p_ble_evt   Bluetooth stack event.
  * 
  */
-void BtleUart::_btleEventHandler(ble_evt_t * p_ble_evt)
+void BtleSerial::_btleEventHandler(ble_evt_t * p_ble_evt)
 {
   ret_code_t err_code;
   
   ble_conn_params_on_ble_evt(p_ble_evt);
   ble_nus_on_ble_evt(&m_nus, p_ble_evt);
   ble_advertising_on_ble_evt(p_ble_evt);
+  ble_conn_state_on_ble_evt(p_ble_evt);
 
   switch (p_ble_evt->header.evt_id)
   {
@@ -365,22 +400,56 @@ void BtleUart::_btleEventHandler(ble_evt_t * p_ble_evt)
   }
 }
 
-bool BtleUart::write(uint8_t* buffer, uint32_t length)
+bool BtleSerial::write(uint8_t* buffer, uint32_t length)
 {
-    if( length <= BLE_NUS_MAX_DATA_LEN )
-    {
-        ble_nus_string_send(&m_nus, buffer, length);
-        return true;
-    }
+    if( length <= BLE_NUS_MAX_DATA_LEN && ble_nus_string_send(&m_nus, buffer, length) == NRF_SUCCESS ) return true;
     else return false;
 }
 
-bool BtleUart::isConnected()
+bool BtleSerial::write(const char* in, const unsigned int length)
 {
-    return ble_conn_state_n_peripherals() > 0;
+    if( length <= BLE_NUS_MAX_DATA_LEN && ble_nus_string_send(&m_nus, (uint8_t*)in, length) == NRF_SUCCESS ) return true;
+    else return false;
 }
 
-void BtleUart::_btleDataHandler(ble_nus_t* p_nus, uint8_t* data, uint16_t length)
+bool BtleSerial::write(const char in[])
 {
-    ringBuffer.write(data, length);
+    return write(in, strlen(in));
+}
+
+bool  BtleSerial::write(int32_t in)
+{
+    if(in == 0)
+    {
+        return putChar('0');
+    }
+    else
+    {
+        bool sign = false;
+        char str[64] = { 0 }; 
+        int16_t i = 62;
+        if (in < 0) 
+        {
+            sign = true;
+            in = abs(in);
+        }
+
+        while (in != 0 && i > 0) 
+        { 
+            str[i--] = (in % 10) + '0';
+            in /= 10;
+        }
+
+        if (sign) str[i--] = '-';
+        return write(str + i + 1, 64-(i+1));
+    }
+}
+bool BtleSerial::putChar(const char c)
+{
+    return write(&c,1);
+}
+
+bool BtleSerial::isConnected()
+{
+    return ble_conn_state_n_peripherals() > 0;
 }
--- src/btleserial.h
+++ src/btleserial.h
@@ -41,10 +41,14 @@ extern "C"
   //  #include "bsp_btn_ble.h"
 }
 
-class BtleUart
+#include "serial.h"
+
+class BtleSerial
 {
     
 private:
+    
+    Serial* _serial;
     
     void _bleStackInit();
     void _advertisingInit();
@@ -55,14 +59,18 @@ private:
     static void _disconnected(ble_gap_evt_t const * const p_gap_evt);
     static void _onAuthorizeRequest(ble_evt_t const * p_ble_evt);
     static void _btleEventHandler(ble_evt_t * p_ble_evts);
-    static void _connParamsErrorHandler(uint32_t nrf_error){} //yes I'm serious
+    static void _connParamsErrorHandler(uint32_t nrf_error){APP_ERROR_HANDLER(nrf_error);}
     static void _connParamsEvtHandler(ble_conn_params_evt_t * p_evt);
     static void _btleDataHandler(ble_nus_t * p_nus, uint8_t * p_data, uint16_t length);
     
 public:
-    BtleUart();
+    BtleSerial(Serial* serial);
     static void start();
     bool dataIsWaiting();
     bool write( uint8_t* buffer, uint32_t length);
+    bool write(const char* in, const unsigned int length);
+    bool write(const char in[]);
+    bool write(int32_t in);
+    bool putChar(const char c);
     bool isConnected();
 };
